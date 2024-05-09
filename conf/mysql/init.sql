dbd44e23ab27e40ec49b6072f234b9bb68e8a411
zhoulin <zhoulin@17vision.com>
2024-05-09 17:31:34 +0800

    大报恩寺文件地址配置

--- conf/mysql/init.sql
+++ conf/mysql/init.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE IF NOT EXISTS holovision CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE DATABASE IF NOT EXISTS vision_resource CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS dabaoensi CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
